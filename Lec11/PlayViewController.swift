0fd739976c6b1f6a5d84ede24eba89f00949bd40
roeyHonig <roeyhonig94@gmail.com>
2018-05-28 11:09:06 +0300

    first commit

---
+++ Lec11/PlayViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PlayViewController.swift
+//  Lec11
+//
+//  Created by hackeru on 14 Sivan 5778.
+//  Copyright © 5778 student.roey.honig. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    @IBAction func play(_ sender: UIButton) {
+        // include a video
+        // import AVKit
+        // url for the video
+        // instantiate an object of AVPlayer
+        
+        //
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Lec11/ViewController.swift
+++ Lec11/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pickImage(_ sender: UIButton) {
+        // instinatie an object
+        // eventually we would want to present it
+        let picker = UIImagePickerController()
+        
+        // we want to be the delegate (delegate is kund like a listener in android)
+        picker.delegate = self
+        
+        // present the ViewController
+        present(picker, animated: true) // no need for the completion block (we can omit the last parmeter)
+    }
+    @IBOutlet weak var galeryImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +33,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
+    
+    //
 }
 
+// these are the protocole needed to be implementes in this "self" viewController
+// in order to be the delegate
+extension ViewController : UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) { // info is an array of key \ values
+        // since we are the delegate we have to tell him when to dismiss
+        print(info) // we're looking at all the key\values and looking for a particulat one
+        
+        // because the key \ values dictionary (info) can hold many keys and many values of diffrent type, it's up to me to cast it
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        galeryImageView.image = image
+        
+        
+        picker.dismiss(animated: true)
+    }
+    
+    
+}
